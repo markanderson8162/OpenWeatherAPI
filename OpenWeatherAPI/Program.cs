cdc907764660945953e85674ab1517d2c647f1c8
Mark Anderson <markanderson8162@gmail.com>
2021-07-13 22:30:05 -0500

    finished open weather exercise

---
+++ OpenWeatherAPI/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+
+namespace OpenWeatherAPI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var client = new HttpClient();
+			Console.WriteLine("Please enter your API key:");
+			var apiKey = Console.ReadLine();
+
+			while (true)
+			{
+				Console.WriteLine();
+				Console.WriteLine("Please enter your city name: ");
+				var cityName = Console.ReadLine();
+				var weatherURL = $"http://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}&units=imperial";
+
+				var response = client.GetStringAsync(weatherURL).Result;
+				var formattedResponse = JObject.Parse(response).GetValue("main").ToString();
+				Console.WriteLine(formattedResponse);
+				Console.WriteLine();
+				Console.WriteLine("Do you want another city?");
+				var userInput = Console.ReadLine();
+				Console.WriteLine();
+
+				if(userInput.ToLower() == "no")
+				{
+					break;
+				}
+
+			}
+		}
+	}
+}
